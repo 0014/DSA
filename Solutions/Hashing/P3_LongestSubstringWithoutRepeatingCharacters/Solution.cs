659c3c2131eb8ae5fbb478edf13b8c08778e5313
Arif Gencosmanoglu <a.gencosmanoglu@gmail.com>
2025-07-27 16:57:34 -0400

    Hashing related solutions before bonus questions

---
+++ Solutions/Hashing/P3_LongestSubstringWithoutRepeatingCharacters/Solution.cs
@@ -0,0 +1,27 @@
+﻿// Given a string s, find the length of the longest substring without duplicate characters.
+
+namespace DSA.Solutions.Hashing.P3_LongestSubstringWithoutRepeatingCharacters
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var set = new HashSet<char>();
+
+            var left = 0;
+            var ans = 0;
+            for (var right = 0; right < s.Length; right++)
+            {
+                while (left < right && set.Contains(s[right]))
+                {
+                    set.Remove(s[left]);
+                    left++;
+                }
+                set.Add(s[right]);
+                ans = Math.Max(ans, right - left + 1);
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ Solutions/Hashing/P771_JewelsAndStones/Solution.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * LeetCode Problem 771: Jewels and Stones
+ * https://leetcode.com/problems/jewels-and-stones/
+ * You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+ * Letters are case sensitive, so "a" is considered a different type of stone from "A".
+ */
+
+namespace DSA.Solutions.Hashing.P771_JewelsAndStones
+{
+    public class Solution
+    {
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            var set = new HashSet<char>();
+            foreach (var c in jewels) set.Add(c);
+
+            var ans = 0;
+            foreach (var c in stones) if (set.Contains(c)) ans++;
+            return ans;
+        }
+    }
+}
